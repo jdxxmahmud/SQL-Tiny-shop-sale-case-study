ea40f16f13113c13370192c1b963fbe65c846075
jdxxmahmud <junaidmahmud@iut-dhaka.edu>
2023-10-12 16:13:22 +0100

    Data and Table creation

---
+++ Data Insertion/products.sql
@@ -0,0 +1,15 @@
+INSERT INTO products (product_id, product_name, price) 
+VALUES
+(1, 'Product A', 10.00),
+(2, 'Product B', 15.00),
+(3, 'Product C', 20.00),
+(4, 'Product D', 25.00),
+(5, 'Product E', 30.00),
+(6, 'Product F', 35.00),
+(7, 'Product G', 40.00),
+(8, 'Product H', 45.00),
+(9, 'Product I', 50.00),
+(10, 'Product J', 55.00),
+(11, 'Product K', 60.00),
+(12, 'Product L', 65.00),
+(13, 'Product M', 70.00);
